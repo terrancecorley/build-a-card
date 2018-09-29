7e7ffbe680378a1fd008c95c5c5609303b87a4c4
Terrance Corley <terrancecorley@outlook.com>
2018-09-28 17:21:04 -0700

    added user form

--- src/MainHeader.js
+++ src/MainHeader.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 function MainHeader(props) {
   return(
     <header>
-      <h1>Build-A-Card</h1>
+      <div className="site-logo__wrapper">
+        <h1 className="site-logo">build-a-card</h1>
+      </div>
     </header>
   );
 }
--- src/UserInfo.js
+++ src/UserInfo.js
@@ -2,7 +2,19 @@ import React, { Component } from 'react';
 
 function UserInfo(props) {
   return(
-    <p>this is user info</p>
+    <div className="user-info__wrapper">
+      <h2>enter your information</h2>
+      <form action="POST">
+        <input type="text" aria-label="full name"/>
+        <input type="text" aria-label="job title"/>
+        <input type="url" aria-label="website url"/>
+        <input type="phone" aria-label="phone number"/>
+        <input type="email" aria-label="email"/>
+        <input type="file" aria-label="business card image"/>
+        <input type="reset" value="Reset" />
+        <button>Download</button>
+      </form>
+    </div>
   );
 }
 
