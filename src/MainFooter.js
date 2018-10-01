2cd19303abd8e2caec2165242605f86a2976efa3
Terrance Corley <terrancecorley@outlook.com>
2018-10-01 16:40:26 -0700

    styled footer

--- src/MainFooter.js
+++ src/MainFooter.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import './css/MainFooter.css';
 
 function MainFooter(props) {
   return(
     <footer>
-      <p>&copy; 2018 Made with &hearts; by <a href="https://terrancecorley.com" target="_blank" rel="noopener">Terrance Corley</a>.</p>
+      <p>&copy; 2018 Made with <span>&hearts;</span> by <a href="https://terrancecorley.com" target="_blank" rel="noopener">Terrance Corley</a>.</p>
     </footer>
   );
 }
