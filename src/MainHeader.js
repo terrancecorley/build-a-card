6a4fa8af7a16e50b79f605bb745a7eebf4b0b151
Terrance Corley <terrancecorley@outlook.com>
2018-10-09 16:50:19 -0700

    added styled components - styled site header

--- src/MainHeader.js
+++ src/MainHeader.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import './css/MainHeader.css';
+// import './css/MainHeader.css';
+import styled from 'styled-components/macro';
+
+const Header = styled.header`
+  font-size: 1.5rem;
+  padding-top: 4em;
+`;
+
+const LogoWrapper = styled.div`
+  text-align: center;
+  border: 4px solid #F0CF65;
+  border-radius: 4px;
+  background-color: #fefefe;
+  margin: 0 auto;
+  width: 20em;
+`;
+
+const Logo = styled.h1`
+  font-family: 'Mali', cursive
+`;
+
 
 function MainHeader(props) {
   return(
-    <header className="site-header">
-      <div className="site-logo__wrapper">
-        <h1 className="site-logo">Build-A-Card</h1>
-      </div>
-    </header>
+    <Header>
+      <LogoWrapper>
+        <Logo>Build-A-Card</Logo>
+      </LogoWrapper>
+    </Header>
   );
 }
 
